e1904391e91d7811c1c3726e31be2ccbdb5693e9
fatimahnadiaekaputri <fatimahnadiaep@gmail.com>
2025-06-30 13:50:04 +0700

    feat: init database

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,33 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-dotenv.config()
+const { Pool } = require('pg');
+dotenv.config();
 
 const cors = require('cors');
-// const connectDB = require('./config/db');
 // const router = require('./src/routes/routes');
 const PORT = process.env.PORT || 5000;
 
-// connectDB();
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
+
+pool.connect((err, client, release) => {
+    if(err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release();
+        if(err) {
+            return console.error("Error executing query", err.stack);
+        }
+        console.log("Database connected: ", result.rows);
+    });
+});
 
 app.use(cors());
 app.use(express.json());
