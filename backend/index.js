3735a3d01155917fa6676995f7abbb760734a64f
fatimahnadiaekaputri <fatimahnadiaep@gmail.com>
2025-06-30 08:01:12 +0700

    feat: init proyek backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config()
+
+const cors = require('cors');
+// const connectDB = require('./config/db');
+// const router = require('./src/routes/routes');
+const PORT = process.env.PORT || 5000;
+
+// connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Test Backend Website Keliling-Keling!');
+});
+// app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
