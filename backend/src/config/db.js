e0d16935c765da1ec312d3ac4d7e39a58090e296
fatimahnadiaekaputri <fatimahnadiaep@gmail.com>
2025-07-19 21:33:51 +0700

    fix: deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('./knexfile')
-const environment = 'development';
+// const environment = 'development';
 
-const db = knex(config[environment]);
+const db = knex(config[process.env.NODE_ENV]);
 module.exports = db;
