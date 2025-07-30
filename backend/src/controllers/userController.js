7eb42cd945a4288085bf9a21a4701553fcc42b37
fatimahnadiaekaputri <fatimahnadiaep@gmail.com>
2025-07-31 04:23:34 +0700

    fix cors

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -57,7 +57,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 *1000
         })
 
--- frontend-cms-nyoba/cms-keliling-keling/src/app/layout.tsx
+++ frontend-cms-nyoba/cms-keliling-keling/src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-=======
->>>>>>> 646c6fcae30e920ab2a974542bede904db69e608
+;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
